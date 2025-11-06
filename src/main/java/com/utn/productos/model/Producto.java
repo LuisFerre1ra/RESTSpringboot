224b28c1eb9409f7c1a2ccc125a08aa62484e8d9
Luis <lferreiraarg@gmail.com>
2025-11-06 18:55:47 -0300

    Configuración y modelo de datos

---
+++ src/main/java/com/utn/productos/model/Producto.java
@@ -0,0 +1,22 @@
+package com.utn.productos.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String nombre;
+    String descripcion;
+    Double precio;
+    Integer stock;
+
+    @Enumerated(EnumType.STRING)
+    Categoria categoria;
+}
